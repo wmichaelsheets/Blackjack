cdc65f6518508eb6710651ae5dddc6036c312a07
Mike Sheets <wmichaelsheets@gmx.com>
2024-07-05 19:20:39 -0400

    Changed call to sum amount.
    Changed call to sum amount, removed emojis from message text, and made the language more correct.

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 let firstCard = 10
-let secondCard = 7
+let secondCard =7
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
 
 function startGame () {
 if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
+    message = "Do you want to draw a new card?"
 } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
+    message = "You've got Blackjack!"
     hasBlackJack = true
 } else {
-    message = "You're out of the game! 😭"
+    message = "You're out of the game!"
     isAlive = false
 }
 
 //console.log(hasBlackJack)
 //console.log(isAlive)
 
-console.log(message)
+messageEl.textContent = message
+sumEl.textContent = "Sum: " + sum
 }
